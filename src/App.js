88d5003d562daf239952b177bba0d1b14584d400
Kanjanapas <oshin_2544@hotmail.com>
2022-07-03 23:34:44 +0700

    create conditions for loading & error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import './App.css'
 import MoreDetailWeather from './components/MoreDetailWeather'
 import ShowWeather from './components/ShowWeather'
@@ -9,7 +9,9 @@ const URL = 'http://api.openweathermap.org/data/2.5'
 
 function App() {
   const [searchText, setSearchText] = useState('')
-  const [weatherData, setWeatherData] = useState({})
+  const [weatherData, setWeatherData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [errMessage, setErrMessage] = useState('')
 
   useEffect(() => {}, [])
 
@@ -17,11 +19,20 @@ function App() {
     setSearchText(e.target.value)
   }
 
-  const handleSearchWeather = (e) => {
+  const handleSearchWeather = async (e) => {
     e.preventDefault()
     if (!searchText) return
-    fetchWeather()
-    setSearchText('')
+    setIsLoading(true)
+    try {
+      await fetchWeather()
+      setSearchText('')
+      setErrMessage('')
+    } catch (error) {
+      console.error('Error❌:', error?.response?.data)
+      setErrMessage(error?.response?.data?.message)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   async function fetchWeather() {
@@ -39,6 +50,25 @@ function App() {
   const location = `${weatherData?.name}, ${weatherData?.sys?.country}`
   return (
     <div className="App">
+      {errMessage && (
+        <div
+          style={{
+            display: 'flex'
+          }}
+        >
+          <div
+            style={{
+              padding: '1.5rem',
+              border: 'crimson 1px solid',
+              borderRadius: '0.5rem',
+              width: 225,
+              margin: 'auto'
+            }}
+          >
+            <p>{errMessage}</p>
+          </div>
+        </div>
+      )}
       <form onSubmit={handleSearchWeather}>
         <input
           type="text"
@@ -48,25 +78,36 @@ function App() {
         />
         <button type="submit">Search</button>
       </form>
-      <ShowWeather
-        temp={weatherData?.main?.temp}
-        cloudIcon={weatherData.weather?.[0]?.icon}
-        location={location}
-      />
-      <MoreDetailWeather
-        highLow={highLowTemp}
-        humidity={weatherData?.main?.humidity}
-        pressure={weatherData?.main?.pressure}
-        visibility={weatherData?.visibility}
-        wind={Math.round(weatherData?.wind?.gust)}
-        windDirection={weatherData?.wind?.deg}
-        sunriseTime={new Date(
-          weatherData?.sys?.sunrise * 1000
-        ).toLocaleTimeString()}
-        sunsetTime={new Date(
-          weatherData?.sys?.sunset * 1000
-        ).toLocaleTimeString()}
-      />
+
+      {(weatherData || isLoading) && isLoading ? (
+        <div style={{ height: '100%', width: '100%', color: 'lightcoral' }}>
+          Loading...
+        </div>
+      ) : (
+        !errMessage && (
+          <Fragment>
+            <ShowWeather
+              temp={weatherData?.main?.temp}
+              cloudIcon={weatherData.weather?.[0]?.icon}
+              location={location}
+            />
+            <MoreDetailWeather
+              highLow={highLowTemp}
+              humidity={weatherData?.main?.humidity}
+              pressure={weatherData?.main?.pressure}
+              visibility={weatherData?.visibility}
+              wind={Math.round(weatherData?.wind?.gust)}
+              windDirection={weatherData?.wind?.deg}
+              sunriseTime={new Date(
+                weatherData?.sys?.sunrise * 1000
+              ).toLocaleTimeString()}
+              sunsetTime={new Date(
+                weatherData?.sys?.sunset * 1000
+              ).toLocaleTimeString()}
+            />
+          </Fragment>
+        )
+      )}
     </div>
   )
 }
